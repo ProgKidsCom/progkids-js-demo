cf32a4f84f40009290402e95d0a3d11e15645ad6
ovchinnikov.av <ov4innikov.av@gmail.com>
2022-05-31 20:21:41 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,32 @@
+const { setBlock } = require('progkids/lib/world');
+const { getPos } = require('progkids/lib/player');
+const { connect } = require('progkids/lib/server');
+const Blocks = require('progkids/lib/blocks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const main = async () => {
+  try {
+    await connect({
+      nick: process.env.NICK,
+      token: process.env.TOKEN
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  setInterval(async () => {
+    const result = await getPos();
+
+    const { ret } = result;
+    if (ret && ret.length === 3) {
+      await setBlock(ret[0], ret[1], ret[2], Blocks.STONE);
+      console.log('done!');
+    } else {
+      console.log(result);
+    }
+  }, 5000);
+};
+
+main();
